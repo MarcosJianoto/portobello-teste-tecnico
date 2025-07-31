a10a954c804606a615e7c7f761100a4a3ea5f332
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-07-31 13:53:04 -0300

    codigo corrigido

--- portobello-back/src/main/java/com/testtechportobello/config/CorsConfig.java
+++ portobello-back/src/main/java/com/testtechportobello/config/CorsConfig.java
@@ -9,8 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("*")
+            .allowedHeaders("*");
     }
 }
